87493ee245a5e30a7f5fdc1e1a34f8dd4c94d5cc
PeachDoors <taylorpterrill@gmail.com>
2022-10-27 12:26:13 -0600

    server.js

--- models/User.js
+++ models/User.js
@@ -30,5 +30,6 @@ const userSchema = new mongoose.Schema({
 
 // Schema Settings
 
+
 // Create a virtual called friendCount that retrieves the length of the user's 
 // friends array field on query.
