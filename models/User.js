89004dea964f590c89bcc4d81d9226ed918bd2e6
PeachDoors <taylorpterrill@gmail.com>
2022-10-28 07:18:45 -0600

    finish user model

--- models/User.js
+++ models/User.js
@@ -18,21 +18,28 @@ const userSchema = new mongoose.Schema({
     },
     thoughts: [
         {
-            // Array of `_id` values referencing the `Thought` model
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
         },
     ],
     friends: [
         {
-            // Array of `_id` values referencing the `User` model (self-reference)
+            type: Schema.Types.ObjectId,
+            ref: 'User'
         },
     ],
-})
+},
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
 
-// Schema Settings
-
-
-// Create a virtual called friendCount that retrieves the length of the user's 
-// friends array field on query.
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
 
 const User = mongoose.model('User', userSchema);
 
