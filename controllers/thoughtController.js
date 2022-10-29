59373ca5a78a3dac95f59309853e73dbb66b6593
PeachDoors <taylorpterrill@gmail.com>
2022-10-29 13:28:49 -0600

    work on thought controller

---
+++ controllers/thoughtController.js
@@ -0,0 +1,57 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    getThoughts(req, res) {
+        Thought.find()
+            .then((thought) => res.json(thought))
+            .catch((err) => res.status(500).json(err));
+    },
+    getSingleThought(req, res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'No thought with that ID' })
+                    : res.json(thought)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    createThought(req, res) {
+        Thought.create(req.body)
+            .then((thought) => {
+                return User.findOneAndUpdate(
+                    { _id: req.body.userId },
+                    { $push: { thoughts: thought._id } },
+                    { new: true }
+                );
+            })
+            .then((user) => {
+                !user
+                    ? res.status(404).json({ message: 'Thought created, but no user found with this ID...' })
+                    : res.json(user)
+            })
+            .catch((err) => res.status(500).json(err));
+    },
+    //todo
+    updateUser(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user with this id!' })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    //todo
+    deleteUser(req, res) {
+        User.findOneAndDelete({ _id: req.params.userId })
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user with that ID' })
+                    : res.json(user)
+            )
+    }
+}
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,3 +1,18 @@
 const router = require('express').Router();
+const {
+    getThoughts,
+    createThought,
+    getSingleThought,
+} = require('../../controllers/thoughtController');
+
+router
+    .route('/')
+    .get(getThoughts)
+    .post(createThought);
+
+router
+    .route('/:thoughtId')
+    .get(getSingleThought)
+    ;
 
 module.exports = router
